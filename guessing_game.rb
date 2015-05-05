98e956797b3abc4e279944afea52575d222ef246
JohnDGreiner <john.d.greiner@gmail.com>
2015-05-05 14:45:14 -0400

    check for duplicate guesses

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,12 +1,26 @@
 #checks guess against answer
 def check_guess(answer,guess)
   if guess == answer
-    puts "You win, you must have cheated."
+    puts "You win, you must have cheated.\n\n"
     exit
   elsif guess > answer
-    puts "Too High."
+    puts "Too High.\n\n"
+
   else
-    puts "Too Low."
+    puts "Too Low.\n\n"
+  end
+end
+
+#check if used guess before
+def dup_check(guesses)
+  if guesses.count != 1
+    counter = guesses.count-1
+    counter.times do |counter|
+      if guesses.last == guesses[counter]
+        puts "You entered #{guesses.last} twice. This game is too difficult for you. Goodbye.\n\n"
+        exit
+      end
+    end
   end
 end
 
@@ -14,12 +28,14 @@ try = 0
 guesses = []
 # pick a random number
 answer = rand(1..100)
+
 (0..4).each do |try|
   # get guess
   puts "Guess my number: "
   puts "This is the answer: #{answer}"
   guesses << gets.chomp.to_i
-puts guesses[0]
+  dup_check(guesses)
   check_guess(answer,guesses[try])
-
 end
+
+puts "Can't believe you didn't guess #{answer}. You LOSE.\n\n"
