2cc9dcda80b945f7da89d49bce93124848bfc892
JohnDGreiner <john.d.greiner@gmail.com>
2015-05-05 13:16:20 -0400

    check guess against answer

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,6 +1,18 @@
+#checks guess against answer
+def check_guess(answer,guess)
+  if guess == answer
+    return puts "You win, you must have cheated."
+    exit
+  elsif guess > answer
+    puts "Too High."
+  else
+    puts "Too Low."
+  end
+end
 
 # pick a random number
 answer = rand(1..100)
 # get guess
 puts "Guess my number: "
-guess = gets.chomp
+guess = gets.chomp.to_i
+check_guess(answer,guess)
