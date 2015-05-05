47fabc2c93032c3494ef184707e76ec6fa03b2e8
JohnDGreiner <john.d.greiner@gmail.com>
2015-05-05 15:23:57 -0400

    check guess to see if < or > previous guesses

--- guessing_game.rb
+++ guessing_game.rb
@@ -11,15 +11,29 @@ def check_guess(answer,guess)
   end
 end
 
-#check if used guess before
+# check if used guess before
 def dup_check(guesses)
-  if guesses.count != 1
-    counter = guesses.count-1
-    counter.times do |counter|
-      if guesses.last == guesses[counter]
-        puts "You guessed #{guesses.last}. I think if you try it one more time it will work.\n\n"
-        break
-      end
+  counter = guesses.count-1
+  counter.times do |counter|
+    if guesses.last == guesses[counter]
+      puts "You guessed #{guesses.last}. I think if you try it one more time it will work.\n\n"
+      break
+    end
+  end
+end
+
+# check inteligence of user
+def guess_accuracy(guesses, answer)
+  numbers = ["first", "second", "third", "fourth"]
+  counter = guesses.count-1
+  counter.times do |counter|
+    if guesses.last < guesses[counter] && answer > guesses[counter]
+      puts "Is #{guesses.last} greater than the #{guesses[counter]} you tried on your #{numbers[counter]} guess?\n\n"
+      break
+    elsif guesses.last > guesses[counter] && answer < guesses[counter]
+      puts "Is #{guesses.last} less than the #{guesses[counter]} you tried on your #{numbers[counter]} guess?\n\n"
+      break
+
     end
   end
 end
@@ -28,13 +42,17 @@ try = 0
 guesses = []
 # pick a random number
 answer = rand(1..100)
+#answer = now.seconds.
 
 (0..4).each do |try|
   # get guess
   puts "Guess my number: "
-  #puts "This is the answer: #{answer}"
+  # puts "This is the answer: #{answer}"
   guesses << gets.chomp.to_i
-  dup_check(guesses)
+  if guesses.count != 1
+    dup_check(guesses)
+    guess_accuracy(guesses, answer)
+  end
   check_guess(answer,guesses[try])
 end
 
